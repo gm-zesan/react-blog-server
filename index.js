fd097bc4404ddb6ef7fbb8c4eb621b0ed8a7c7c5
Zesan <gmzesan7767@gmail.com>
2022-10-01 03:46:20 +0600

    api development done

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+const cors = require("cors");
+app.use(cors());
+
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -14,9 +17,10 @@ const categoryRouter = require("./routes/categories.js");
 
 const port = process.env.PORT || 5000;
 const mongoose = require("mongoose");
+const path = require("path");
 
 app.use(express.json());
-
+app.use("/images", express.static(path.join(__dirname, "/images")));
 mongoose
     .connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
@@ -25,19 +29,19 @@ mongoose
     .then(console.log("DB CONNCETED"))
     .catch((err) => console.log(err));
 
-    const storage = multer.diskStorage({
-        destination: (req, file, cb) => {
-            cb(null, "images");
-        },
-        filename: (req, file, cb) => {
-            cb(null, req.body.name);
-        },
-    });
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, req.body.name);
+    },
+});
 
-    const upload = multer({ storage: storage });
-    app.post("/api/upload", upload.single("file"), (req, res) => {
-        res.status(200).json("File has been uploaded");
-    });
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRouter);
